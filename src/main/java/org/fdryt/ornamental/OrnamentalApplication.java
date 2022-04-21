69720c6b65224f9a842476b4ac5198e5073ff4a4
Jose Maria <josmariaguilar@gmail.com>
2022-04-21 16:03:31 -0400

    Initial Project

---
+++ src/main/java/org/fdryt/ornamental/OrnamentalApplication.java
@@ -0,0 +1,13 @@
+package org.fdryt.ornamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrnamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrnamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fdryt/ornamental/OrnamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fdryt.ornamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrnamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
