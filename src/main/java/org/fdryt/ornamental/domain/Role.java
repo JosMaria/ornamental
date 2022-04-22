f420012a4bf5d2369586118cdb5b472ec135b9bf
Jose Maria <josmariaguilar@gmail.com>
2022-04-22 11:06:33 -0400

    added package domain

---
+++ src/main/java/org/fdryt/ornamental/domain/Role.java
@@ -0,0 +1,24 @@
+package org.fdryt.ornamental.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
