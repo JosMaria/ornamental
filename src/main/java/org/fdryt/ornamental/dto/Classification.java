6ce5476e87811ff036da64bcdf0246097625b53c
Jose Maria <josmariaguilar@gmail.com>
2022-05-07 18:09:21 -0400

    data filling in family and classification

--- src/main/java/org/fdryt/ornamental/dto/Classification.java
+++ src/main/java/org/fdryt/ornamental/dto/Classification.java
@@ -1,4 +1,8 @@
 package org.fdryt.ornamental.dto;
 
 public enum Classification {
+
+    ORNAMENTAL, FORESTAL, INDUSTRIAL,
+    ALIMENTICIA, MEDICINAL, EXÓTICA,
+    CACTU, FRUTAL, CRASA, SUCULENTA, ALIMENTICIO
 }
