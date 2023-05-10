f59d566678023e32785a7f917454b378c0d37929
JoseMaria <josmariaguilar@gmail.com>
2023-05-09 20:43:20 -0400

    add configuration to cors

--- src/main/java/org/fdryt/ornamental/controller/FamilyController.java
+++ src/main/java/org/fdryt/ornamental/controller/FamilyController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
-@CrossOrigin(origins = "http://localhost:5173/", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/families")
--- src/main/java/org/fdryt/ornamental/security/SecurityConfiguration.java
+++ src/main/java/org/fdryt/ornamental/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(POST, "/api/v1/auth/authenticate").permitAll();
                     auth.requestMatchers("/api/v1/products/**").permitAll();
