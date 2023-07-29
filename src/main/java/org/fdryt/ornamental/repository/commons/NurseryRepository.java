4a6265504770d7bc9198a825c851a8d73fe73293
JoseMaria <josmariaguilar@gmail.com>
2023-07-28 22:57:39 -0400

    create entity manager

---
+++ src/main/java/org/fdryt/ornamental/repository/commons/NurseryRepository.java
@@ -0,0 +1,54 @@
+package org.fdryt.ornamental.repository.commons;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+/**
+ * An Interface that represents the Data Access Layer of the application.
+ * Most of the common methods are defined here.
+ *
+ * @param <TEntity> represents the type of the concrete entity managed by JPA.
+ * @param <ID> represents the identifier of the entity which should be serializable.
+ */
+public interface NurseryRepository<TEntity, ID extends Serializable> {
+
+    /**
+     * It verifies if the entity for the given ID exists.
+     *
+     * @param id the entity identifier.
+     * @return TRUE if it exists and FALSE if it does not.
+     */
+    boolean exists(ID id);
+
+    /**
+     * Gets the entity for the given ID. If it does not exist, then it will return an empty {@link Optional} result.
+     *
+     * @param id the entity identifier.
+     * @return {@link Optional} of the TEntity
+     */
+    Optional<TEntity> findById(ID id);
+
+    /**
+     * Adds a new record to the database table based on the given entity.
+     *
+     * @param entity the new record with the required information.
+     * @return the persisted entity.
+     */
+    TEntity add(TEntity entity);
+
+    /**
+     * Updates an existing entity with the new information.
+     *
+     * @param entity the entity to update.
+     * @return the updated entity.
+     */
+    TEntity update(TEntity entity);
+
+    /**
+     * Deletes an existing entity based on the ID passed as parameter.
+     *
+     * @param id the entity ID.
+     */
+    void deleteById(ID id);
+
+}
---
+++ src/main/java/org/fdryt/ornamental/repository/impl/MyPlantRepositoryImpl.java
@@ -0,0 +1,17 @@
+package org.fdryt.ornamental.repository.impl;
+
+import jakarta.persistence.EntityManager;
+import org.fdryt.ornamental.domain.plant.MyPlant;
+import org.fdryt.ornamental.repository.MyPlantRepository;
+import org.fdryt.ornamental.repository.commons.AbstractNurserySqlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyPlantRepositoryImpl extends AbstractNurserySqlRepository<MyPlant, Integer> implements MyPlantRepository {
+
+    @Autowired
+    public MyPlantRepositoryImpl(EntityManager em) {
+        super(em, MyPlant.class);
+    }
+}
