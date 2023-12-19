7d468092b08dd46ee989b2e75a9e5a9db5ef2ad6
JoseMaria <josmariaguilar@gmail.com>
2023-12-19 18:04:57 -0400

    add domain to the cross origin

--- src/main/java/org/fdryt/ornamental/controller/UserController.java
+++ src/main/java/org/fdryt/ornamental/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.fdryt.ornamental.dto.user.UserResponseDTO;
 import org.fdryt.ornamental.service.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:5173/", "https://magnificent-daifuku-409cab.netlify.app/"}, allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
