1b44cccd38a4615772eab182efd03be26edf9cd7
JosMaria <josmariaguilar@gmail.com>
2024-06-29 09:20:32 -0400

    obtain all items

--- src/main/java/org/fdryt/ornamental/repository/RepertoryRepository.java
+++ src/main/java/org/fdryt/ornamental/repository/RepertoryRepository.java
@@ -1,8 +1,8 @@
 package org.fdryt.ornamental.repository;
 
-import org.fdryt.ornamental.domain.plant.Plant;
+import org.fdryt.ornamental.domain.plant.alternative.PlantV3;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepertoryRepository extends JpaRepository<Plant, String> {}
+public interface RepertoryRepository extends JpaRepository<PlantV3, String> {}
--- src/main/java/org/fdryt/ornamental/service/impl/RepertoryServiceImpl.java
+++ src/main/java/org/fdryt/ornamental/service/impl/RepertoryServiceImpl.java
@@ -1,14 +1,19 @@
 package org.fdryt.ornamental.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.fdryt.ornamental.domain.plant.alternative.PlantV3;
 import org.fdryt.ornamental.dto.repertory.ItemResponseDTO;
 import org.fdryt.ornamental.repository.RepertoryRepository;
 import org.fdryt.ornamental.service.RepertoryService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RepertoryServiceImpl implements RepertoryService {
 
@@ -16,9 +21,20 @@ public class RepertoryServiceImpl implements RepertoryService {
 
     @Override
     public List<ItemResponseDTO> obtainAllItems() {
-        ItemResponseDTO itemOne = new ItemResponseDTO("Calatea zebrina", "Calathea zebrina", "Marantaceae");
-        ItemResponseDTO itemTwo = new ItemResponseDTO("Calatea zebrina", null, null);
-        ItemResponseDTO itemThree = new ItemResponseDTO("Calatea zebrina", null, "Marantaceae");
-        return null;
+        List<PlantV3> plantsObtained = repertoryRepository.findAll();
+        log.info("Obtained all plants to show in the repertory");
+
+        return plantsObtained.stream()
+                .map(plant -> {
+                    String scientificName = null;
+                    if (plant.getScientificName() != null && !plant.getScientificName().isEmpty()) {
+                        scientificName = plant.getScientificName().substring(0, 1).toUpperCase() + plant.getScientificName().substring(1);
+                        if (plant.getDiscoverer() != null) {
+                            scientificName += " " + Character.toUpperCase(plant.getDiscoverer());
+                        }
+                    }
+                    return new ItemResponseDTO(plant.getCommonName(), scientificName, plant.getFamily().getName());
+                    })
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
